2c5de2c84fff7eb5c281fdc420c5cad469b14c9c
mmtol <mmigueltolosana@gmail.com>
2026-03-18 09:16:00 +0100

    Add project files.

---
+++ MarvelComicsMMT/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MarvelComicsMMT
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ MarvelComicsMMT/Models/Comic.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace MarvelComicsMMT.Models
+{
+    public class Comic
+    {
+        [JsonProperty("id")]
+        public int IdComic { get; set; }
+
+        [JsonProperty("title")]
+        public string Titulo { get; set; }
+
+        [JsonProperty("detailUrl")]
+        public string Url { get; set; }
+
+        [JsonProperty("seriesName")]
+        public string Serie { get; set; }
+
+        [JsonProperty("yearPage")]
+        public int Year { get; set; }
+    }
+}
